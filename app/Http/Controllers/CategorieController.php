4cdc840fd6d2712084196afd31411ce1ce5fe8f3
azogba <azogbayawagloria@gmail.com>
2024-03-04 11:12:35 +0000

    affichage reussi

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -18,8 +18,7 @@ class CategorieController extends Controller
     // }
      public function categorie(){
         $categorie=Categorie::latest()->get();
-        //::all();
-        //$salle = $salle->sortByDesc('created_at');
+    
         return view('categorielist',compact('categorie'));
     }
 
@@ -51,24 +50,20 @@ class CategorieController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-   public function edit(Categorie $categorie)
+   public function edit(string $id)
 {
+    $categorie = Categorie::findOrFail($id);
+
     return view('editecategorie', compact('categorie'));
 }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update( categorie $categorie)
-    {
-        $request->validate([
-        'libelle' => 'required|unique:categories|max:25',
-        // Ajoutez d'autres validations selon vos besoins 
-    ]);
-   
+    public function update( Request  $request, string $id)
+    { 
         Categorie::Where('id',$id)->update([
-        'libelle'=>$request->input('libelle')]);
-        
+        'libelle'=>$request->input('libelle')]);  
        return redirect('/categorielist')->with('success', 'La modification a été effectuer avec succes');
     }
 
@@ -81,6 +76,6 @@ class CategorieController extends Controller
     public function destroy(string $id)
     {
        Categorie::destroy($id);
-     return redirect('/categorie')->with('success', 'Suppression effectuer');
+     return redirect('/categorielist')->with('success', 'Suppression effectuer');
     }
 }
